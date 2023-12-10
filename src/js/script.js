aa31fa6a45bef73db071c35857ba2c7160bc66e9
rafa7lako <103770511+rafa7lako@users.noreply.github.com>
2023-12-10 19:33:44 +0100

    finished the feature section

--- src/js/script.js
+++ src/js/script.js
@@ -14,8 +14,19 @@ $navToggler.addEventListener('click', () => {
  * Header scroll state
  */
 
-const /** {NodeElement} */ $header = document.querySelector("[data-header}");
+const /** {NodeElement} */ $header = document.querySelector("[data-header]");
 
 window.addEventListener('scroll', e => {
     $header.classList[window.scrollY > 50 ? 'add' : "remove"]( "active")
 });
+
+
+// Add to favourite button toggle
+
+const /** {NodeList} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active")
+    });
+})
